112c99f985922777ad5f944bc65819e5ca64d97d
hans <913951422@qq.com>
2017-12-17 20:18:41 +0800

    finish lab

--- ensemble.py
+++ ensemble.py
@@ -1,4 +1,8 @@
+#coding: utf-8
 import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
@@ -10,23 +14,56 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
-        pass
+        self.weak_clf = weak_classifier
+        self.n_weakers_limit = n_weakers_limit
 
-    def is_good_enough(self):
-        '''Optional'''
-        pass
+        #sample weights
+        self.w = None
+        #error rate: classifier weights
+        self.alpha_list = []
+        
+        self.error_list = [] #list for storing error rate during iteration
+        self.weak_clf_list = [] #list for storing weak base classifiers during iteration
 
-    def fit(self,X,y):
+        self.validation_score_list = []
+
+    def fit(self,X,y, X_val=None, y_val=None):
         '''Build a boosted classifier from the training set (X, y).
-
+        
         Args:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
+            validation set(X_val, y_val): Used for early stopping.
         '''
-        pass
+        #initialize sample weights
+        self.w = np.ones(X.shape[0])/X.shape[0]
 
+        for i in range(self.n_weakers_limit):
+            cur_clf = self.weak_clf.fit(X, y, sample_weight=self.w)
+            predict_res = cur_clf.predict(X)
+            cur_error = np.sum((predict_res != y.reshape(-1,)) * self.w)
+            print(i,'base clf training accuracy: ', np.mean(y.reshape(-1,) == predict_res))
+            if cur_error > 0.5:#base classifer is not good enough, discard it
+                continue
+            elif cur_error == 0:
+                print('This base classifer has training error equals 0. End of iteration.')
+                break
 
-    def predict_scores(self, X):
+            self.weak_clf_list.append(cur_clf)
+            self.error_list.append(cur_error)
+
+            cur_alpha = 1/2 * np.log((1-cur_error)/cur_error)
+            self.alpha_list.append(cur_alpha)
+
+            self.w *= np.exp(-cur_alpha * y.reshape(-1,) * predict_res)
+            self.w /= np.sum(self.w)
+
+            if (X_val is not None) & (y_val is not None):
+                res = np.mean(self.predict(X_val)==y_val)
+                self.validation_score_list.append(res)
+                print('current validation accuracy: ', res)
+
+    def predict_scores(self, X, use_first_k_clfs):
         '''Calculate the weighted sum score of the whole base classifiers for given samples.
 
         Args:
@@ -35,10 +72,17 @@ class AdaBoostClassifier:
         Returns:
             An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
         '''
-        pass
+        res = np.zeros((X.shape[0]))
 
-    def predict(self, X, threshold=0):
-        '''Predict the catagories for geven samples.
+        k = len(self.weak_clf_list)
+        if use_first_k_clfs >= 0:
+            k = use_first_k_clfs
+        for i in range(k):
+            res += self.weak_clf_list[i].predict(X) * self.alpha_list[i]
+        return res
+
+    def predict(self, X, threshold=0, use_first_k_clfs=-1):#可以选择使用前k棵决策树进行预测（根据验证集的结果）
+        '''Predict the catagories for given samples.
 
         Args:
             X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
@@ -47,7 +91,26 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        pass
+        res = self.predict_scores(X, use_first_k_clfs)
+        res[res >= threshold]=1
+        res[res < threshold]=-1
+        return res.reshape(-1,1)
+
+    def plotting(self):
+        plt.title('Adaboost')
+        plt.xlabel('iteration number')
+        plt.ylabel('validation accuracy')
+        plt.plot(range(len(self.validation_score_list)), self.validation_score_list)  
+        plt.grid()
+        plt.show()
+
+    def get_report(self, X_test, y_test):
+        #get the index of the highest value in validation_score_list
+        idx = self.validation_score_list.index(max(self.validation_score_list))
+        pred_res = self.predict(X_test, use_first_k_clfs=-1)
+        with open('report.txt', "wb") as f:
+            repo=classification_report(y_test,pred_res,target_names=["face","nonface"])
+            f.write(repo.encode())
 
     @staticmethod
     def save(model, filename):
--- train.py
+++ train.py
@@ -1,6 +1,57 @@
+#coding: utf-8
+import os
+from PIL import Image #Python Imaging Library
+import pickle
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from feature import NPDFeature
+from ensemble import AdaBoostClassifier
 
+def to_gray_resize(path,x=None,y=None):
+    for file in os.listdir(path):
+        file_path=os.path.join(path,file)
+        label=None
+        if path=='datasets/original/face/':
+            label=[1]
+        elif path=='datasets/original/nonface/':
+            label=[-1]
+        image=Image.open(file_path).convert('L').resize((24,24)) #打开图片并转换成灰度图
+        if (x is None) & (y is None):#不能使用==：==是element-wise的
+            x=np.array([NPDFeature(np.asarray(image)).extract()])
+            y=np.array([label])
+        else:
+            #把是脸的和非脸的图片都拼接起来
+            x=np.vstack((x,NPDFeature(np.asarray(image)).extract()))#与np.concat([x1, x2], axis=0)等价
+            y=np.vstack((y,label))
+    return x,y
+
+def preprocess():
+    x,y=to_gray_resize('datasets/original/face/')
+    x,y=to_gray_resize('datasets/original/nonface/',x,y)
+
+    #write binary
+    with open('datasets/features/feature', 'wb') as file:
+        pickle.dump(x, file)
+    with open('datasets/features/label', 'wb') as file:
+        pickle.dump(y, file)
+    print(x.shape,y.shape)
 
 if __name__ == "__main__":
-    # write your code here
-    pass
+    print('loading data...')
+    # preprocess()
+    with open('datasets/features/feature', 'rb') as file:
+        x = pickle.load(file)
+    with open('datasets/features/label', 'rb') as file:
+        y = pickle.load(file)
 
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+    print('start training...')
+    ada_clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth = 3),5)
+    ada_clf.fit(X_train, y_train, X_test, y_test)
+    ada_clf.plotting()
+
+    print('Writing report...')
+    ada_clf.get_report(X_test, y_test)
